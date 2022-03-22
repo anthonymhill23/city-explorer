94151a838207e2d54eb7e0868928b238cb6dc8fe
Anthony Hill <anthonymhill23@gmail.com>
2022-03-22 00:30:45 -0400

    added necessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import 
+import server from './server.js'
+import { Button, Form } from 'react-bootstrap';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>City</Form.Label>
+        <Form.Control type="city" placeholder="Enter City" />
+      
+      </Form.Group>
+
+      
+
+      
+      <Button variant="primary" type="submit">
+        Explore!!
+      </Button>
+    </Form>
   );
 }
 
--- src/server.js
+++ src/server.js
@@ -1,39 +1,10 @@
-'use strict';
 
-require('dotenv').config();
 
-const express = require('express');
-const cors = require('cors');
-const weatherData = require('./data/weather.json')
-const app = express();
-
-app.use(cors());
-
-const PORT = process.env.PORT
-
-app.get('/test', function (request, response) {
-    response.send('test works');
-})
-
-app.get('/weather-data', function getWeatherData(request, response) {
-    let queriedData = weatherData.find(city => city.city_name.toLowerCase() === request.query.city_name.toLowerCase());
-    if (queriedData) {
-        let finalClientData = queriedData.data.map(dataPoint => new ReturnData(dataPoint));
-        response.status(200).send(finalClientData);
-    } else {
-        response.status(400).send('no data for that city');
-    }
-})
-
-app.get('/*', function (request, response) {
-    response.status(404).send('error loading request');
-})
-
-class ReturnData {
-    constructor(serverSideData) {
-        this.date = serverSideData.datetime,
-            this.desc = `High: ${serverSideData.max_temp}°C Low: ${serverSideData.low_temp}°C, with ${serverSideData.weather.description.toLowerCase()}`
-    }
+// this is constructor for query the cities
+function Location(location, locationData) {
+  this.search_query = location;
+  this.formatted_query = locationData[0].display_name;
+  this.latitude = locationData[0].lat;
+  this.longitude = locationData[0].lon;
 }
 
-app.listen(PORT, () => console.log('server is listening on ', PORT))
