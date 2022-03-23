f8971735dc664bd91ec657eac2e12945de1a60bb
Anthony Hill <anthonymhill23@gmail.com>
2022-03-23 19:44:48 -0400

    added correct API key

--- src/App.js
+++ src/App.js
@@ -3,67 +3,78 @@ import axios from 'axios';
 import Weather from './Weather.js';
 import './App.css';
 import Map from './Map'
-export default class App extends Component {
+import { Card } from 'react-bootstrap';
+class App extends React.Component {
 
-constructor(props) {
-  super(props);
-  this.state = {
-    queryCity: '',
-    locationObject: {},
-    imageURL: '',
-    error: false,
-    weather: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      queryCity: '',
+      locationObject: {},
+      imageURL: '',
+      error: false,
+      weather: []
+    }
   }
-}
-getLocation = async() => {
-  try {
-    let result = await axios.get(`https://us1.locationiq.com/v1/search.php?key=pk.pk.e2fd6814fb09c9f71384ce4ee890ca0c=${this.state.queryCity}&format=json`);
-      // console.log(result);
-      this.setState({ locationObject: result.data[0], error: false}, this.getWeather);
-  } catch (error) {
-    console.error(error);
-    console.log('there was an error');
-    this.setState({ error: true })
+  getLocation = async (e) => {
+    try {
+      let result = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_APIKEY}&q=${this.state.queryCity}&format=json`);
+      console.log(result);
+      this.setState({ locationObject: result.data[0], error: false }, this.getWeather);
+    } catch (error) {
+      console.error(error);
+      console.log('there was an error');
+      this.setState({ error: true })
+    }
   }
-}
-getWeather = async() => {
-  let city = this.state.locationObject.display_name.split(',')[0];
-  let url = `${process.env.REACT_APP_URL}/weather?city_name=${city}`;
-  try {
-    let results = await axios.get(url);
-  this.setState({weather: results.data, error: false})
-  } catch(error) {
-    this.setState({ error: true })
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ queryCity: e.target.city.value }, this.getLocation);
   }
-}handleSubmit = (e) => {
-  e.preventDefault();
-  this.setState({ queryCity: e.target.city.value }, this.getLocation);
-}
 
   render() {
-    return(
+    return (
       <div id="content-wrapper">
         <form id='form' onSubmit={this.handleSubmit}>
           <input type="text" placeholder="city name" name="city" />
           <button id='button' type="submit">Explore!</button>
         </form>
         <div>
-        <div id="city-data">
-          {this.state.locationObject.display_name ?
-          <>
-            <p>{this.state.locationObject.display_name}</p>
-            <Map locationObject={this.state.locationObject}/>
-          </>
-           :
-           <p>Search for a city to explore</p>}
-          {this.state.weather.length > 0 && <Weather weather={this.state.weather}/>}
-        </div>
-        <div id="image-wrapper">
-        </div>
-        
-        {this.state.error && <p>There was an error with your request</p>}
+          <div id="city-data">
+            {this.state.locationObject.display_name ?
+            
+                <Map
+                  locationObject={this.state.locationObject} />
+              
+              :
+              <p>Search for a city to explore</p>}
+            {this.state.weather.length > 0 && <Weather weather={this.state.weather} />}
+          </div>
+          <div id="image-wrapper">
+          </div>
+
+          {this.state.error && <p>There was an error with your request</p>}
         </div>
       </div>
     )
   }
+
+
+
+
 }
+
+
+
+
+// getWeather = async () => {
+//   let city = this.state.locationObject.display_name.split(',')[0];
+//   let url = `${process.env.REACT_APP_APIKEY}/weather?city_name=${city}`;
+//   try {
+//     let results = await axios.get(url);
+//     this.setState({ weather: results.data, error: false })
+//   } catch (error) {
+//     this.setState({ error: true })
+//   }
+// } 
+export default App;
