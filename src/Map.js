e12942b51195668ac29a87c4b850efb9fd9e820c
Anthony Hill <anthonymhill23@gmail.com>
2022-03-23 19:45:37 -0400

    added card function to map js

--- src/Map.js
+++ src/Map.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
-
+import { Card } from 'react-bootstrap'
 export default class Map extends Component {
   render() {
     return (
-      <div>
-        <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${this.props.locationObject.lat},${this.props.locationObject.lon}&zoom=12`} alt={this.props.locationObject.display_name}/>
-      </div>
+
+<Card style={{ width: '18rem' }}>
+  
+  <Card.Img variant="top" src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_APIKEY}&center=${this.props.locationObject.lat},${this.props.locationObject.lon}&zoom=12`} />
+  <Card.Body>
+    <Card.Title>{this.props.locationObject.display_name}</Card.Title>
+    <Card.Text> latitude{this.props.locationObject.lat}
+                longitude{this.props.locationObject.lon}
+    
+    </Card.Text>
+    
+  </Card.Body>
+</Card>
+  
     )
   }
 }
